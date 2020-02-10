d4c3ddc4199bc560e31f95a41b5839e210bc965f
TusharK54 <tushar.54k@gmail.com>
2020-02-10 17:33:19 -0500

    refactoring

--- data-exploration.py
+++ data-exploration.py
@@ -3,12 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-plt.scatter(np.arange(0,10), np.arange(10,0, -1))
-plt.show()
-
 # %%
 data = pd.read_csv(r'data/train.csv')
 data_top = data.head() 
 data_top
-
-# %%
