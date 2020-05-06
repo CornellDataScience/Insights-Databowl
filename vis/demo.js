f8c769e136522782090ebe29bf3a14c3cebc4c75
TusharK54 <tushar.54k@gmail.com>
2020-05-06 11:20:11 -0400

    final push

--- vis/demo.js
+++ vis/demo.js
@@ -20,28 +20,69 @@ function loadJSON(filePath, callback) {
 }
 
 class PlayerBody extends Dynamics.DynamicBody {
-    constructor(position, velocity, color) {
-        const shape = new Geometry.Circle(3);
-        const material = new Dynamics.Material(0.5);
-        super(position, velocity, shape, material);
-
-        this.hitboxColor = color;
-        this.velocityColor = color;
-        this.addCollisionHandler(() => {})
-    }
-}
-
-loadJSON('play.json', play => {
-    play.forEach(player => {
+    constructor(player) {
+        // get data
         const position = new Vector(player.X, (53.3-player.Y)).scale(8);
     
         const direction = (player.Dir + 90) % 360
         const velocity = new Vector(player.S * Math.cos(direction), -player.S * Math.sin(direction)).scale(8);
-        const color = player.IsRusher ? 'green' : player.Team ? 'red' : 'blue';
-    
-        const body = new PlayerBody(position, velocity, color);
+
+        const weight = player.PlayerWeight;
+
+        // set attributes
+        const shape = new Geometry.Circle(3);
+        const material = new Dynamics.Material(0.2, 1, weight);
+        super(position, velocity, shape, material);
+
+        this.player = player;
+
+        this.color = player.IsRusher ? 'green' : player.Offense ? 'red' : 'blue';;
+        this.hitboxColor = this.color;
+        this.velocityColor = this.color;
+      //  this.addCollisionHandler(() => {})
+    }
+
+    get acceleration() {
+        if (this.player.Offense) {
+            return new Vector(this.player.A, 0).scale(8)
+        }
+        return super.acceleration;
+    }
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.shape.x, this.shape.y, this.shape.r, 0, 2*Math.PI);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.arc(this.shape.x, this.shape.y, this.shape.r*2, 0, 2*Math.PI);
+        ctx.strokeStyle = this.color;
+        ctx.stroke();
+    }
+}
+
+loadJSON('play.json', play => {
+    const metadata = play[0];
+
+    world.draw = (ctx) => {
+        // endzones
+        ctx.fillStyle = 'black';
+        ctx.fillRect(10*8, 0, 1, 120*8);
+        ctx.fillRect(110*8, 0, 1, 120*8);
+
+        // scrimage
+        ctx.fillStyle = 'red';
+        ctx.fillRect((110-metadata.YardLine)*8, 0, 1, 120*8);
+
+        // yards
+        ctx.fillStyle = 'blue';
+        ctx.fillRect((110-metadata.YardLine+metadata.Yards)*8, 0, 1, 120*8);
+    };
+
+    play.forEach(player => {
+        const body = new PlayerBody(player);
         world.addBody(body);
-    
         console.log(body, player)
     });
 });
